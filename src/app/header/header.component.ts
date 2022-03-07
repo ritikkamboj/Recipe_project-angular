e4e9044fb7a07d48a29c8c2a6932a9301c068400
ritik4314 <ritikkamboj4314@gmail.com>
2022-03-07 11:44:49 +0530

    Initial commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+})
+// yeh neeche waala typescript part hai
+export class HeaderComponent {}
